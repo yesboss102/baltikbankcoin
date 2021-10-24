5b9d59eca541cf9aaef9a624727e3d71a47bf8f9
yesboss102 <yessboss102@yandex.ru>
2021-10-24 16:49:17 +0500

    Update CryptoNoteConfig.h
    genesis change

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "baltikbankcoin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff00019ca79812029b2e4c0281c0b02e7c53291a94d1d0cbff8883f024f5142ee494ffbbd0880712101388fd0645967f32c2bd770824d1f9a911264efd2e8a2bd789210d107cbacc5c";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
